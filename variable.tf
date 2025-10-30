8c672866a36f250d9079aea48fe92ef67f985781
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2025-10-29 21:53:03 -0500

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "cluster_name" {
+    default = "test"
+}
+
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+variable "account_id" {
+    default = "060866400888"
+}
+
+variable "secret_name" {
+    default = "testing-secrets-manager"
+}
