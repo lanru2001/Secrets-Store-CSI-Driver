a9cfcf2c6ada2ff0a914f2169332ad1be0cf19c9
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2025-11-12 13:28:02 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,14 @@
+# EKS Cluster Name 
 variable "cluster_name" {
     default = "test"
 }
 
+# AWS Region 
 variable "aws_region" {
     default = "us-east-1"
 }
 
+# AWS Account ID 
 variable "account_id" {
-    default = "xxxxxxxxxxxxxx"
+    default = "xxxxxxxxxxxxxx" 
 }
